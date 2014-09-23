da36401bbec7567ecfbfb4385ac8a514ee1d1a8c
mhkeller <michael.keller@gmail.com>
2014-09-22 23:20:26 -0400

    more granular x axis

--- js/spotted-tail.js
+++ js/spotted-tail.js
@@ -7,9 +7,9 @@ function spottedTail() {
 	var customTimeFormat = d3.time.format.multi([
 		['.%L', function(d) { return d.getMilliseconds(); }],
 		// [':%S', function(d) { return d.getSeconds(); }],
-		// ['%I:%M', function(d) { return d.getMinutes(); }],
-		// ['%I %p', function(d) { return d.getHours(); }],
-		// ['%a %d', function(d) { return d.getDay() && d.getDate() != 1; }],
+		['%I:%M', function(d) { return d.getMinutes(); }],
+		['%I %p', function(d) { return d.getHours(); }],
+		['%a %d', function(d) { return d.getDay() && d.getDate() != 1; }],
 		['%b %e', function(d) { return d.getDate() != 1; }],
 		['%b', function(d) { return d.getMonth(); }],
 		['%Y', function() { return true; }]
